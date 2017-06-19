8b8c3f310c2190e7c78f75320fdfb6d3d522eb02
Ricardo Corrales Barquero <ricorrales07@gmail.com>
2017-06-19 13:58:06 -0600

    Inicio

---
+++ Redes/Proyecto/AS.java
@@ -0,0 +1,13 @@
+import java.net.Socket;
+/**
+ * Write a description of class AS here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AS
+{
+    private Socket mySocket;
+    
+    public static void main () {}
+}
