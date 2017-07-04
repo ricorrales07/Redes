e34bc2bc0c5323ec8fe27d4ee2af4780ebd87cd6
Mariajo Leal <mariajo20leal@gmail.com>
2017-07-03 22:12:23 -0600

    Update PaqueteAlcanzabilidad.java

--- Redes/Proyecto/PaqueteAlcanzabilidad.java
+++ Redes/Proyecto/PaqueteAlcanzabilidad.java
@@ -11,37 +11,26 @@ import java.net.*;
 public class PaqueteAlcanzabilidad
 {
     Paquete_t tipo;
-	NumeroAS as;
-	byte [] cantDestinos;
-	Destino [] listaDestinos; 
-	
+    NumeroAS as;
+    Destino [] listaDestinos; 
     
     public PaqueteAlcanzabilidad(Paquete_t tipo, byte[] paquete)
     {
         this.tipo = tipo;
         try
         {
-            as = new NumeroAS(Arrays.copyOfRange(paquete, 0, 2));
-	    cantDestinos = new byte [(Arrays.copyOfRange(paquete, 2, 6))];	
-            ip = InetAddress.getByAddress(Arrays.copyOfRange(paquete, 6, 10));
-            mascara = InetAddress.getByAddress(Arrays.copyOfRange(paquete, 10, 14));
-	    listaDestinos = new Destino[(Arrays.copyOfRange(paquete, 14, paquete.length)];
+            as = new NumeroAS(Arrays.copyOfRange(paquete, 0 , 2)); 
         }
         catch (IllegalArgumentException e)
         {
             throw new RuntimeException("Esto no debería pasar");
         }
-        catch (UnknownHostException e)
-        {
-            throw new RuntimeException("Esto no debería pasar");
-        }
     }
-	
-    public PaqueteAlcanzabilidad(Paquete_t tipo, NumeroAS as, byte [] cantDestinos, Destino [] listaDestinos)
+    
+    public PaqueteAlcanzabilidad(Paquete_t tipo, NumeroAS as, Destino [] listaDestinos)
     {
         this.tipo = tipo;
         this.as = as;
-        this.cantDestinos = cantDestinos;
         this.listaDestinos = listaDestinos;
     }
     
@@ -55,14 +44,10 @@ public class PaqueteAlcanzabilidad
     {
         return as;
     }
-	
-    public byte[] getCantDest()
-    {
-    	return cantDestinos;
-    }	
-	
+    
+    
     public Destino[] getListaDestinos()
     {
-    	return listaDestinos;
+        return listaDestinos;
     }
 }
