fb63c2a86fe2f1b68fa16f2bc46df080e2135b51
Mariajo Leal <mariajo20leal@gmail.com>
2017-07-03 22:12:56 -0600

    Update TablaAlcanzabilidad.java

--- Redes/Proyecto/TablaAlcanzabilidad.java
+++ Redes/Proyecto/TablaAlcanzabilidad.java
@@ -2,6 +2,7 @@ import java.util.logging.*;
 import java.util.Hashtable;
 import java.net.*;
 import java.io.IOException;
+import java.util.Arrays;
 
 // <>
 
@@ -13,33 +14,36 @@ import java.io.IOException;
  */
 public class TablaAlcanzabilidad
 {
-    private class PaqueteAlcanzabilidad
+    private class Dest
     {
-		
-		private Paquete_t tipo;
-		private NumeroAS as;
-		private byte [] cantDestinos;
-		private Destino [] listaDestinos; 
-        
-        public PaqueteAlcanzabilidad(Paquete_t tipoV,NumeroAS asVecino, byte [] cantDV, Destino [] listaDV)
+		private InetAddress ip;
+    	private InetAddress mascara;
+    	private NumeroAS [] as;
+        public Dest(byte[] paquete)
         {
-            tipo = tipoV;
-	    as = asVecino;
-            cantDestinos = cantDV;
-            listaDestinos = listaDV;
+            try
+            {
+                ip = InetAddress.getByAddress(Arrays.copyOfRange(paquete, 6, 10));
+                mascara = InetAddress.getByAddress(Arrays.copyOfRange(paquete, 10, 14));
+            }
+            catch (IllegalArgumentException e)
+            {
+            	throw new RuntimeException("Esto no debería pasar");
+            }
+            catch (UnknownHostException e)
+            {
+                throw new RuntimeException("Esto no debería pasar");
+            }
         }
-
     }
     
-    private Hashtable<InetAddress, PaqueteAlcanzabilidad> tabla;
-    
+    private Hashtable<InetAddress, Dest> tabla;
     private Logger registro;
     private FileHandler fHandler;
     private SimpleFormatter sFormatter;
-    
     // patrón singleton
     private static TablaAlcanzabilidad tablaUnica;
-    
+
     private TablaAlcanzabilidad() throws IOException
     {
         fHandler = new FileHandler("logTablaAlcnzabilidad.txt");
@@ -50,7 +54,7 @@ public class TablaAlcanzabilidad
         registro.addHandler(fHandler);
         registro.setLevel(Level.INFO);
         
-        tabla = new Hashtable<InetAddress, PaqueteAlcanzabilidad>();        
+        tabla = new Hashtable<InetAddress, Dest>();        
     }
     
     public static synchronized TablaAlcanzabilidad getTabla() throws IOException
@@ -60,15 +64,9 @@ public class TablaAlcanzabilidad
         return tablaUnica;
     }
     
-    /*public synchronized void addPaquete(PaqueteAlcanzabilidad pa)
+    public synchronized void addPaquete(InetAddress ipVecino ,byte [] paquete)
     {
-        addPaquete(pa.getAS(),pa.getCantDest(), pa.getListaDestinos());
-    }*/
-   
-    
-    public synchronized void addPaquete(InetAddress ipVecino,Paquete_t tipo,NumeroAS as, byte [] cantDestinos, Destino [] listaDestinos)
-    {
-        PaqueteAlcanzabilidad paqueteNuevo = new PaqueteAlcanzabilidad(tipo,as,cantDestinos,listaDestinos);
+        Dest paqueteNuevo = new Dest(paquete);
         tabla.put(ipVecino, paqueteNuevo);
         registro.log(Level.INFO, "Nuevo paquete de alcanzabilidad añadido a la tabla de alcanzabilidad");
     }
